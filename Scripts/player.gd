6c882de9b7ed2f47eae38f16ecc6593ebb8f0c1b
MangoWizard <arda.ozden@protonmail.com>
2025-11-15 00:23:04 +0000

    player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,27 @@
 extends CharacterBody2D
 
 
-@export var ACC: float = 300.0
+@export var ACC_RATIO : float = 2
+@export var DECEL_RATIO: float = 4
 @export var SPEED_CAP: float = 1000.0 
 
 func _physics_process(delta: float) -> void:
-	var h_dir = Input.get_axis("ui_left", "ui_right")
-	var v_dir = Input.get_axis("ui_up", "ui_down")
+	var accel = ACC_RATIO*SPEED_CAP
+	var decel = DECEL_RATIO*SPEED_CAP
 	
+	var x = Input.get_axis("ui_left", "ui_right")
+	var y = Input.get_axis("ui_up", "ui_down")
 	
-
+	var dir : Vector2 = Vector2(x, y).normalized()
+	var dv = accel*delta*dir
+	
+	velocity += dv
+	if (velocity + dv).length() >= SPEED_CAP:
+		velocity = velocity.normalized() * SPEED_CAP
+		
+	if x == 0:
+		velocity.x = move_toward(velocity.x, 0, delta*decel)
+	if y == 0:
+		velocity.y = move_toward(velocity.y, 0, delta*decel)
+	 
 	move_and_slide()
